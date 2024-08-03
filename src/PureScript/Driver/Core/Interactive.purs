87fc67e25593927a33ded1e9a3fd13d6506349a7
Justin Garcia <purefunctor@gmail.com>
2024-08-04 01:21:22 +0800

    feature: API for file diagnostics

--- src/PureScript/Driver/Core/Interactive.purs
+++ src/PureScript/Driver/Core/Interactive.purs
@@ -6,11 +6,14 @@ import Control.Monad.ST (ST)
 import Control.Monad.ST.Ref (STRef)
 import Control.Monad.ST.Ref as STRef
 import Data.Either (Either(..))
+import Data.Map (Map)
+import Data.Map as Map
 import Data.Maybe (Maybe(..))
 import Data.Set (Set)
 import Data.Set as Set
 import Data.Traversable (for_)
 import Partial.Unsafe (unsafeCrashWith)
+import PureScript.CST.Errors (ParseError)
 import PureScript.CST.Types (ModuleName)
 import PureScript.Diagnostics.Types (Diagnostic(..), DiagnosticKind(..))
 import PureScript.Driver.Files (ParsedFile, parseFile, parsedImports, parsedModuleName)
@@ -24,7 +27,7 @@ import PureScript.Utils.Mutable.GraphMap as GraphMap
 newtype Interactive r = Interactive
   { engine ∷ Engine r
   , graph ∷ GraphMap r ModuleNameId
-  , diagnostics ∷ STRef r (Set Diagnostic)
+  , parseErrors ∷ STRef r (Map String ParseError)
   }
 
 updateGraph ∷ ∀ r. Interactive r → ModuleNameId → ParsedFile → ST r Unit
@@ -38,17 +41,21 @@ updateGraph (Interactive { engine: Engine { stable }, graph }) moduleNameId pars
     GraphMap.addNode graph importedModuleNameId
     GraphMap.addEdge graph moduleNameId importedModuleNameId
 
-updateDiagnostics ∷ ∀ r. Interactive r → Diagnostic → ST r Unit
-updateDiagnostics (Interactive { diagnostics }) diagnostic =
-  void $ STRef.modify (Set.insert diagnostic) diagnostics
+insertParseError ∷ ∀ r. Interactive r → String → ParseError → ST r Unit
+insertParseError (Interactive { parseErrors }) filePath parseError = do
+  void $ STRef.modify (Map.insert filePath parseError) parseErrors
+
+deleteParseError ∷ ∀ r. Interactive r → String → ST r Unit
+deleteParseError (Interactive { parseErrors }) filePath = do
+  void $ STRef.modify (Map.delete filePath) parseErrors
 
 createFile ∷ ∀ r. Interactive r → String → String → ST r Unit
 createFile interactive@(Interactive { engine: engine@(Engine { stable }) }) filePath fileSource = do
   case parseFile fileSource of
     Left { error } →
-      updateDiagnostics interactive $
-        Diagnostic { kind: DiagnosticParseError error }
+      insertParseError interactive filePath error
     Right parsedFile → do
+      deleteParseError interactive filePath
       let
         moduleName ∷ ModuleName
         moduleName = parsedModuleName parsedFile
@@ -71,9 +78,9 @@ editFile interactive@(Interactive { engine: engine@(Engine { stable }) }) filePa
           unsafeCrashWith "invariant violated: oldModuleName should have been stabilized."
       case parseFile fileSource of
         Left { error } →
-          updateDiagnostics interactive $
-            Diagnostic { kind: DiagnosticParseError error }
+          insertParseError interactive filePath error
         Right parsedFile → do
+          deleteParseError interactive filePath
           let
             moduleName ∷ ModuleName
             moduleName = parsedModuleName parsedFile
@@ -90,3 +97,16 @@ renameFile (Interactive { engine: Engine { stable } }) oldFilePath newFilePath =
       Stable.editFilePath stable oldFileId newFilePath
     Nothing →
       unsafeCrashWith "invariant violated: oldFilePath should have been stabilized."
+
+fileDiagnostics ∷ ∀ r. Interactive r → String → ST r (Set Diagnostic)
+fileDiagnostics (Interactive { engine: engine@(Engine { stable }), parseErrors }) filePath = do
+  -- Check parseErrors first to avoid using diagnostics from the latest successful parse.
+  STRef.read parseErrors <#> Map.lookup filePath >>= case _ of
+    Just parseError →
+      pure $ Set.singleton $ Diagnostic { kind: DiagnosticParseError parseError }
+    Nothing →
+      Stable.lookupFileId stable filePath >>= case _ of
+        Just fileId →
+          Engine.queryGet @"diagnostics" engine fileId
+        Nothing →
+          unsafeCrashWith "invariant violated: filePath should have been stabilized."
